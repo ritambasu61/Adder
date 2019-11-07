8f022e9ed4f77a53f3c32f1cc988cf18df37ecf6
ritambasu61 <57460456+ritambasu61@users.noreply.github.com>
2019-11-07 16:31:17 +0530

    Add files via upload

---
+++ adder.py
@@ -0,0 +1,23 @@
+"""
+created by: Ritam Basu
+created on:06/11/19
+created for: assignment1.
+
+"""
+from math import *
+def f(x,y):
+    return (x+y)
+#adding two strings
+x= "hello"
+y= "world"
+print ("addition: ",f(x,y))
+#adding two lists
+x= [1,2,3]
+y= [4,5,6]
+print ("addition: ",f(x,y))
+#adding floating  points
+x= float(1.5)
+y= float(2.5)
+print ("addition: ",f(x,y))
+
+# 
